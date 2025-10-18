f9948097da4ccdc693d30ad27a8366bef4bfc68f
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-10-18 13:46:01 +0530

    `latest update`

---
+++ src/config/skills.config.ts
@@ -0,0 +1,64 @@
+export const skillsConfig = [
+  {
+    category: "Frontend",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "TypeScript",
+      "React",
+      "Next.js",
+      "TailwindCSS",
+      "SCSS",
+      "Bootstrap",
+      "Headless UI",
+      "ShadCN UI",
+      "Framer Motion",
+      "Redux",
+      "SWR",
+      "Tanstack Query",
+      "Tanstack Router",
+      "React Hook Form",
+      "NextAuth",
+    ],
+  },
+  {
+    category: "Backend",
+    technologies: [
+      "Node.js",
+      "Express.js",
+      "Microservices (EDA)",
+      "JWT",
+      "OOPs",
+    ],
+  },
+  {
+    category: "Database",
+    technologies: [
+      "MongoDB",
+      "PostgreSQL",
+      "Redis",
+      "RabbitMQ",
+    ],
+  },
+  {
+    category: "Cloud & DevOps",
+    technologies: [
+      "AWS (EC2, Lambda, Beanstalk)",
+      "Docker",
+      "CI/CD",
+      "Vercel",
+      "Firebase",
+      "Supabase",
+      "Neon",
+    ],
+  },
+  {
+    category: "Version Control & Tools",
+    technologies: [
+      "Git",
+      "GitHub",
+      "VS Code",
+    ],
+  },
+];
